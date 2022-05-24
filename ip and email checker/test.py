0c9381dfb71a42e171bee92ab6aae9c69bdd69a5
Emir <emir210515@ogr.duzce.edu.tr>
2022-05-24 14:58:57 +0300

    initial

---
+++ ip and email checker/test.py
@@ -0,0 +1,34 @@
+import checker
+
+def check():
+    inptCounter = 0 
+
+    while inptCounter < 5:
+    
+        userSelect = int(input("Lütfen yapmak istediğiniz işlemi seçiniz;\n1->E-mail\n2->IP Adress\nSeçiminiz : "))
+
+        if userSelect == 1:
+            userEmail = input("Lütfen e-mail'inizi giriniz : ")
+            checker = checker.Checker(userEmail)
+            checker.checkMail()
+            if checker.getMailStatus():
+                print("Geçerli e-mail adresi.")
+            else:
+                print("Geçersiz e-mail adresi.")
+
+        else:
+            userIp = input("Lütfen IP Adresinizi giriniz: ")
+            checker = checker.Checker(ip_address=userIp)
+            checker.checkIp()
+            if checker.getIpStatus():
+                print("Geçerli ip adresi")
+            else:
+                print("Geçersiz ip adresi")
+
+        inptCounter += 1
+
+
+
+
+if __name__ == "__main__":
+    check()
